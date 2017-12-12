6a69175832d2d9ec7c36d787459efa285450dc77
Marc Thomas <hello@mrcthms.com>
2017-12-12 13:04:49 +0000

    Add in final type styles

--- src/App.js
+++ src/App.js
@@ -6,17 +6,71 @@ class App extends Component {
   render () {
     return (
       <div className='app'>
-        <Type element='h1' order='title'>This is a regular title</Type>
-        <Type element='h2' order='title' size='smaller'>This is a smaller title</Type>
-        <Type element='h3' order='subtitle' size='larger'>This is a larger subtitle</Type>
-        <Type element='h4' order='subtitle'>This is a regular subtitle</Type>
-        <Type element='h5' order='subtitle' size='smaller'>This is a smaller subtitle</Type>
-        <Type element='h6' order='subtitle' size='smallest'>This is a smallest subtitle</Type>
-        <Type element='p' order='body' size='larger'>This is a larger body</Type>
-        <Type element='p' order='body'>This is a regular body</Type>
-        <Type element='p' order='body' size='smaller'>This is a smaller body</Type>
-        <Type element='p' order='body' size='smallest'>This is a smallest body</Type>
-        <Type element='div' order='label'>This is a regular label</Type>
+        <Type element='div' order='label'>Text Title</Type>
+        <Type element='h1' order='title'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed.</Type>
+
+        <Type element='div' order='label'>Text Title (w/ Prose)</Type>
+        <Type element='h1' order='title' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed.</Type>
+
+        <Type element='div' order='label'>Text Title Smaller</Type>
+        <Type element='h2' order='title' size='smaller'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin.</Type>
+
+        <Type element='div' order='label'>Text Title Smaller (w/ Prose)</Type>
+        <Type element='h2' order='title' size='smaller' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin.</Type>
+
+        <Type element='div' order='label'>Text Subtitle Larger</Type>
+        <Type element='h3' order='subtitle' size='larger'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore.</Type>
+
+        <Type element='div' order='label'>Text Subtitle Larger (w/ Prose)</Type>
+        <Type element='h3' order='subtitle' size='larger' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore.</Type>
+
+        <Type element='div' order='label'>Text Subtitle</Type>
+        <Type element='h4' order='subtitle'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave.</Type>
+
+        <Type element='div' order='label'>Text Subtitle (w/ Prose)</Type>
+        <Type element='h4' order='subtitle' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave.</Type>
+
+        <Type element='div' order='label'>Text Subtitle Smaller</Type>
+        <Type element='h5' order='subtitle' size='smaller'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave.</Type>
+
+        <Type element='div' order='label'>Text Subtitle Smaller (w/ Prose)</Type>
+        <Type element='h5' order='subtitle' size='smaller' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave.</Type>
+
+        <Type element='div' order='label'>Text Subtitle Smallest</Type>
+        <Type element='h6' order='subtitle' size='smallest'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Subtitle Smallest (w/ Prose)</Type>
+        <Type element='h6' order='subtitle' size='smallest' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body Larger</Type>
+        <Type element='p' order='body' size='larger'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body Larger (w/ Prose)</Type>
+        <Type element='p' order='body' size='larger' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body</Type>
+        <Type element='p' order='body'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body (w/ Prose)</Type>
+        <Type element='p' order='body' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body Smaller</Type>
+        <Type element='p' order='body' size='smaller'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body Smaller (w/ Prose)</Type>
+        <Type element='p' order='body' size='smaller' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body Smallest</Type>
+        <Type element='p' order='body' size='smallest'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Body Smallest (w/ Prose)</Type>
+        <Type element='p' order='body' size='smallest' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore, fixie skateboard chillwave fingerstache air plant succulents try-hard celiac. Lyft tofu forage, listicle craft beer edison bulb beard. Food truck butcher knausgaard, VHS freegan distillery wolf bicycle rights.</Type>
+
+        <Type element='div' order='label'>Text Label</Type>
+        <Type element='div' order='label'>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore.</Type>
+
+        <Type element='div' order='label'>Text Label (w/ Prose)</Type>
+        <Type element='div' order='label' prose>Adaptogen chillwave forage mustache gochujang. Mumblecore skateboard cold-pressed, scenester migas wayfarers austin. Gastropub fashion axe locavore.</Type>
       </div>
     )
   }
